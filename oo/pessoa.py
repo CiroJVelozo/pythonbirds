94963cb3f22a17fee90eba4cf507f944d576cb14
Ciro Jose Velozo Ribeiro <cirojvribeiro@gmail.com>
2020-10-28 12:12:28 -0300

    Criado criando os atributos da classe inserindo um objeto Pesso em um atributo lista filhos de outra classe

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,16 +1,21 @@
 class Pessoa:
-    def __init__(self, nome = None,idade = None):
+    def __init__(self,*filho, nome = None,idade = None):
         self.nome = nome
         self.idade = idade
+        self.filhos = list(filho)
 
     def comprimentar(self):
         return f'Ola {id(self)}'
 
 if __name__ == '__main__':
-     p = Pessoa('Ciro',25)
 
-     print(Pessoa.comprimentar(p))
-     print(id(p))
-     print(p.comprimentar())
-     print(p.nome)
-     print(p.idade)
+     jose = Pessoa()
+
+     ciro = Pessoa(jose,nome='Ciro',idade=25)
+
+     print(Pessoa.comprimentar(ciro))
+     #print(id(ciro))
+     #print(ciro.comprimentar())
+     #print(ciro.nome)
+     #print(ciro.idade)
+     print(ciro.filhos)
