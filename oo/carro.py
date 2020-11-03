9bff4fc4bddf344d32b6e297226fa6a8658bd686
Ciro Jose Velozo Ribeiro <cirojvribeiro@gmail.com>
2020-11-03 13:11:04 -0300

    Criando a classe carro motor e direção e seus atributos

---
+++ oo/carro.py
@@ -0,0 +1,47 @@
+class motor:
+    def __init__(self, velocidade = 0):
+        self.velocidade = velocidade
+
+    def acelerar(self):
+        self.velocidade+=1
+
+    def frear(self):
+
+        if self.velocidade <= 1:
+            self.velocidade = 0
+        else:
+            self.velocidade -= 2
+
+class  direcao:
+
+    def __init__(self, valor = 'N'):
+        self.valor = valor
+
+    def girar_a_direita(self):
+
+        if self.valor == 'N':
+            self.valor ='L'
+        elif self.valor =='L':
+            self.valor = 'S'
+        elif self.valor =='S':
+            self.valor ='O'
+        elif self.valor=='O':
+            self.valor = 'N'
+
+        return self.valor
+
+    def girar_a_esquerda(self):
+
+        if self.valor == 'N':
+            self.valor ='O'
+        elif self.valor =='O':
+            self.valor = 'S'
+        elif self.valor =='S':
+            self.valor ='L'
+        elif self.valor=='L':
+            self.valor = 'N'
+
+        return self.valor
+
+
+if __name__ == '__main__':
